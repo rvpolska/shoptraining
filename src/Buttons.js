090520ba65937d6285f1bff238dcfe1e80f18cd3
RV Group <44459687+rvpolska@users.noreply.github.com>
2023-02-05 18:40:04 +0100

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,9 +1,10 @@
 
 
-function Buttons({filteredClothes}){
+function Buttons({filteredClothes,data,setShop}){
 
     return(
         <div className="container">
+             <button className="button" onClick={()=> setShop(data) (1)}>All</button>
             <button className="button" onClick={()=> filteredClothes ('nike')}>Nike</button>
             <button className="button" onClick={()=> filteredClothes ('reebok')}>Reebok</button>
             <button className="button" onClick={()=> filteredClothes ('underarmour')}>Underarmour</button>
